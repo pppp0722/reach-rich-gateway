c7c0f134c2035c240e46de47f6c21fddca73ad42
ilhwan <poj0722@naver.com>
2023-03-21 19:43:41 +0900

    :tada: 프로젝트 생성

---
+++ README.md
@@ -0,0 +1,3 @@
+## reach-rich-gateway 👤
+가계부 정리, 공유 웹서비스 Reach Rich - API Gateway
+> Reach Rich 서버의 개발 과정은 [블로그](https://velog.io/@pppp0722/series/Reach-Rich-%EA%B0%9C%EB%B0%9C%EA%B8%B0)에 연재되어 있습니다.
---
+++ src/main/java/com/reachrich/reachrichgateway/ReachRichGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.reachrich.reachrichgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReachRichGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReachRichGatewayApplication.class, args);
+    }
+
+}
